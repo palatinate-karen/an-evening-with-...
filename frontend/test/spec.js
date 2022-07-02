4d20bf64fc2d352d7d13a647c47655a2a5c930cd
xcg4012 <karen.s.hahn@web.de>
2022-07-02 14:10:08 +0200

    Sprint#4 E2E-Test erweitert

--- frontend/test/spec.js
+++ frontend/test/spec.js
@@ -1,4 +1,5 @@
 describe('Protractor Demo App', function() {
+  var userName = 'TestUser';
   it('should load start page', function() {
     browser.get('http://localhost:4200/');
 
@@ -6,5 +7,11 @@ describe('Protractor Demo App', function() {
     expect(element(by.id('inputName'))).toBeTruthy();
     expect(element(by.id('kanyeStart')).getTitle()).toEqual('KanyePic');
     expect(element(by.id('trumpStart')).getTitle()).toEqual('TrumpPic');
+  });
+  it('should start game', function () {
+    element(by.id('inputName')).sendKeys(userName);
+    element(by.id('trumpStart')).click();
+
+    expect(element(by.id('quote'))).toBeTruthy();
   });
 });
